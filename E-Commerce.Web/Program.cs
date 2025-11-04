956211e737515f73765bf102304b734e752852a7
Mohamed-Ezzat1907 <HP@DESKTOP-K4F4GJC>
2025-11-04 17:58:22 +0200

    API_Session_01

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,4 +1,7 @@
 
+using E_Commerce.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace E_Commerce.Web
 {
     public class Program
@@ -7,18 +10,28 @@ namespace E_Commerce.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            #region Dependency injection
+
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            #endregion
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+               app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
